fad79ffe6c53ecfbb8b15cf85b742a1932d38a17
Aya Ramirez <ayaramz@gmail.com>
2022-06-28 23:04:39 -0400

    commit tree edb0efe2358ef6f67e5997c9abe90f625e57fe13

---
+++ app/controllers/cheeses_controller.rb
@@ -0,0 +1,13 @@
+class CheesesController < ApplicationController
+
+    def index
+        cheeses = Cheese.all
+        render json: cheeses
+    end
+
+    def show
+        cheese = Cheese.find(params[:id])
+        render json: cheese
+    end
+
+end
